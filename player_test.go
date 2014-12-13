dd74c3345feb25c96419b80b21311cbf09b8d34f
Cody Powell <codypo@gmail.com>
2014-12-13 22:09:29 +0000

    Oooooops, forgot to add tests for verifying the presense of black rooks and knights.

--- player_test.go
+++ player_test.go
@@ -16,6 +16,7 @@ func TestPlayerNewPopulatesPawns(t *testing.T) {
 		}
 	}
 
+	// Verify that the black player's pawns exist.
 	p = NewPlayer(Black)
 
 	for i := 0; i < 8; i++ {
@@ -31,16 +32,31 @@ func TestPlayerNewPopulatesRooks(t *testing.T) {
 	p := NewPlayer(White)
 
 	// First 8 pieces are pawns.  2 immediately subsequent rooks.
-	rook1 := p.pieces[8]
-	r1_loc := rook1.pieceData().square
-	if r1_loc.x != 1 || r1_loc.y != 1 {
-		t.Errorf("Rook 1 is located at %d, %d", r1_loc.x, r1_loc.y)
+	rook := p.pieces[8]
+	r_loc := rook.pieceData().square
+	if r_loc.x != 1 || r_loc.y != 1 {
+		t.Errorf("Rook 1 is located at %d, %d", r_loc.x, r_loc.y)
 	}
 
-	rook2 := p.pieces[9]
-	r2_loc := rook2.pieceData().square
-	if r2_loc.x != 8 || r2_loc.y != 1 {
-		t.Errorf("Rook 2 located at %d, %d", r2_loc.x, r2_loc.y)
+	rook = p.pieces[9]
+	r_loc = rook.pieceData().square
+	if r_loc.x != 8 || r_loc.y != 1 {
+		t.Errorf("Rook 2 located at %d, %d", r_loc.x, r_loc.y)
+	}
+
+	// Now verify that the black player's rooks exist.
+	p = NewPlayer(Black)
+
+	rook = p.pieces[8]
+	r_loc = rook.pieceData().square
+	if r_loc.x != 1 || r_loc.y != 8 {
+		t.Errorf("Rook 3 is located at %d, %d", r_loc.x, r_loc.y)
+	}
+
+	rook = p.pieces[9]
+	r_loc = rook.pieceData().square
+	if r_loc.x != 8 || r_loc.y != 8 {
+		t.Errorf("Rook 4 located at %d, %d", r_loc.x, r_loc.y)
 	}
 }
 
@@ -48,15 +64,16 @@ func TestPlayerNewPopulatesKnights(t *testing.T) {
 	p := NewPlayer(White)
 
 	// First 8 pieces are pawns.  Then rooks, then knights.
-	knight1 := p.pieces[10]
-	k1_loc := knight1.pieceData().square
-	if k1_loc.x != 2 || k1_loc.y != 1 {
-		t.Errorf("Knight 1 is located at %d, %d", k1_loc.x, k1_loc.y)
+	knight := p.pieces[10]
+	k_loc := knight.pieceData().square
+	if k_loc.x != 2 || k_loc.y != 1 {
+		t.Errorf("Knight 1 is located at %d, %d", k_loc.x, k_loc.y)
 	}
 
-	knight2 := p.pieces[11]
-	k2_loc := knight2.pieceData().square
-	if k2_loc.x != 7 || k2_loc.y != 1 {
-		t.Errorf("Knight 2 located at %d, %d", k2_loc.x, k2_loc.y)
+	// TODO: We're missing black tests, you rascist.
+	knight = p.pieces[11]
+	k_loc = knight.pieceData().square
+	if k_loc.x != 7 || k_loc.y != 1 {
+		t.Errorf("Knight 2 located at %d, %d", k_loc.x, k_loc.y)
 	}
 }
