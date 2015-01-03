38c359044b2c1c1efbf89104d1805a887c8d5203
Cody Powell <codypo@gmail.com>
2015-01-03 18:07:42 +0000

    Rooks be movin'.  Code is cleaner and actually does some right stuff now, to boot.

--- rook.go
+++ rook.go
@@ -18,60 +18,62 @@ func (r Rook) generateValidMoves(start Square) []*Square {
 	fmt.Printf("I am in %d, %d...\n", start.x, start.y)
 	moves := make([]*Square, 0)
 
-	// HACK: this is not at all efficient.
-	immediateMoves := make([]*Square, 4)
-	immediateMoves[0] = &Square{x: start.x - 1, y: start.y}
-	immediateMoves[1] = &Square{x: start.x + 1, y: start.y}
-	immediateMoves[2] = &Square{x: start.x, y: start.y + 1}
-	immediateMoves[3] = &Square{x: start.x, y: start.y - 1}
-	immediateOpenings := 0
+	proceedUp := true
+	proceedDown := true
+	proceedLeft := true
+	proceedRight := true
 
-	for _, i := range immediateMoves {
-		status := r.pieceData().evaluateSquare(i)
-		if status == squareVacant {
-			immediateOpenings++
-		} else if status == squareOccupiedByOpponent {
-			immediateOpenings++
+	for i := startSquare; i <= endSquare; i++ {
+		// Evaluate the next move up.
+		if proceedUp {
+			move := &Square{x: start.x, y: start.y + i}
+			status := r.pieceData().evaluateSquare(move)
+			if status == squareVacant || status == squareOccupiedByOpponent {
+				moves = append(moves, move)
+				fmt.Printf("Adding valid move %d, %d\n", move.x, move.y)
+			} else {
+				proceedUp = false
+			}
 		}
-	}
 
-	fmt.Printf("immediateOpenings is of length %d\n", immediateOpenings)
-	if immediateOpenings == 0 {
-		return moves
-	}
-
-	possibleMoves := [endSquare * 2]*Square{}
-	pIndex := 0
-
-	// Rooks can move horizontally or vertically.
-	// Look at the possible horizontal moves.
-	for x := startSquare; x <= endSquare; x++ {
-		possibleMoves[pIndex] = &Square{x: x, y: start.y}
-		pIndex++
-	}
-
-	// Look at the possible vertical moves.
-	for y := startSquare; y <= endSquare; y++ {
-		possibleMoves[pIndex] = &Square{x: start.x, y: y}
-		pIndex++
-	}
-
-	// Can the rook actually move to any of these places?
-	for _, s := range possibleMoves {
-		fmt.Printf("Evaluating possible move %d, %d\n", s.x, s.y)
-		status := r.pieceData().evaluateSquare(s)
-		if status == squareVacant {
-			fmt.Printf(" Appending vacant square\n")
-			moves = append(moves, s)
-		} else if status == squareOccupiedByMe {
-			// Pass.  Castle at some point?
-		} else if status == squareOccupiedByOpponent {
-			fmt.Printf(" Appending occupied square\n")
-			moves = append(moves, s)
+		// Evaluate the next move down.
+		if proceedDown {
+			move := &Square{x: start.x, y: start.y - i}
+			status := r.pieceData().evaluateSquare(move)
+			if status == squareVacant || status == squareOccupiedByOpponent {
+				moves = append(moves, move)
+				fmt.Printf("Adding valid move %d, %d\n", move.x, move.y)
+			} else {
+				proceedDown = false
+			}
 		}
+
+		// Evaluate the next move right.
+		if proceedRight {
+			move := &Square{x: start.x + i, y: start.y}
+			status := r.pieceData().evaluateSquare(move)
+			if status == squareVacant || status == squareOccupiedByOpponent {
+				moves = append(moves, move)
+				fmt.Printf("Adding valid move %d, %d\n", move.x, move.y)
+			} else {
+				proceedRight = false
+			}
+		}
+
+		// Evaluate the next move left.
+		if proceedLeft {
+			move := &Square{x: start.x - i, y: start.y}
+			status := r.pieceData().evaluateSquare(move)
+			if status == squareVacant || status == squareOccupiedByOpponent {
+				moves = append(moves, move)
+				fmt.Printf("Adding valid move %d, %d\n", move.x, move.y)
+			} else {
+				proceedLeft = false
+			}
+		}
+
+		fmt.Printf("Length of moves is %d\n", len(moves))
 	}
 
-	// Still need to catch the fact that rooks can't hop over dudes.
-	fmt.Printf("Length of moves is %d, possibleMoves is %d\n", len(moves), len(possibleMoves))
 	return moves
 }
