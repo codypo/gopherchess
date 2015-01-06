61d901c5e6b64f2e742e59ad1b1ef6ad5359135e
Cody Powell <codypo@gmail.com>
2015-01-06 04:24:20 +0000

    More useful rook tests.

--- rook_test.go
+++ rook_test.go
@@ -19,23 +19,23 @@ func TestRookGeneratesValidMoves(t *testing.T) {
 	fmt.Println("Done checking the starting position.")
 
 	// Sneakiness to force a move without validation.
-	genMoves := rook1.generateValidMoves(Square{x: 1, y: 3})
+	genMoves := rook1.generateValidMoves(Square{x: 4, y: 4})
 
 	var expMoves [11]*Square
-	// Rook can move horizontally across the board.
-	expMoves[0] = &Square{x: 2, y: 3}
-	expMoves[1] = &Square{x: 3, y: 3}
-	expMoves[2] = &Square{x: 4, y: 3}
-	expMoves[3] = &Square{x: 5, y: 3}
-	expMoves[4] = &Square{x: 6, y: 3}
-	expMoves[5] = &Square{x: 7, y: 3}
-	expMoves[6] = &Square{x: 8, y: 3}
 
 	// Rook can move vertically up to the first opponent piece.
-	expMoves[7] = &Square{x: 1, y: 4}
-	expMoves[8] = &Square{x: 1, y: 5}
-	expMoves[9] = &Square{x: 1, y: 6}
-	expMoves[10] = &Square{x: 1, y: 7}
+	// Rook can move horizontally across the board.
+	expMoves[0] = &Square{x: 4, y: 3}
+	expMoves[1] = &Square{x: 4, y: 5}
+	expMoves[2] = &Square{x: 4, y: 6}
+	expMoves[3] = &Square{x: 4, y: 7}
+	expMoves[4] = &Square{x: 1, y: 3}
+	expMoves[5] = &Square{x: 2, y: 3}
+	expMoves[6] = &Square{x: 3, y: 3}
+	expMoves[7] = &Square{x: 5, y: 3}
+	expMoves[8] = &Square{x: 6, y: 3}
+	expMoves[9] = &Square{x: 7, y: 3}
+	expMoves[10] = &Square{x: 8, y: 3}
 
 	if len(genMoves) != len(expMoves) {
 		t.Errorf("Generated moves are of length %d, while expected moves are of length %d", len(genMoves), len(expMoves))
