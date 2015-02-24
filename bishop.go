01f49b1e6895e34d39dd0b0c1f69ea27dbd1ed92
Cody Powell <codypo@gmail.com>
2015-02-24 07:12:04 +0000

    Consolidated a lot of common code into piece.go.

--- bishop.go
+++ bishop.go
@@ -13,57 +13,5 @@ func (b Bishop) pieceData() *PieceData {
 }
 
 func (b Bishop) generateValidMoves(start Square) []*Square {
-	moves := make([]*Square, 0)
-
-	proceedUpLeft := true
-	proceedDownLeft := true
-	proceedUpRight := true
-	proceedDownRight := true
-
-	for i := startSquare; i <= endSquare; i++ {
-		// Evaluate the next move up and to the left.
-		if proceedUpLeft {
-			move := &Square{x: start.x - i, y: start.y + i}
-			status := b.pieceData().evaluateSquare(move)
-			if status == squareVacant || status == squareOccupiedByOpponent {
-				moves = append(moves, move)
-			}
-
-			// You can only proceed onward if you're looking at a vacant square.
-			proceedUpLeft = (status == squareVacant)
-		}
-
-		// Evaluate the next move down and to the left.
-		if proceedDownLeft {
-			move := &Square{x: start.x - i, y: start.y - i}
-			status := b.pieceData().evaluateSquare(move)
-			if status == squareVacant || status == squareOccupiedByOpponent {
-				moves = append(moves, move)
-			}
-
-			proceedDownLeft = (status == squareVacant)
-		}
-
-		// Evaluate the next move up and to the right.
-		if proceedUpRight {
-			move := &Square{x: start.x + i, y: start.y + i}
-			status := b.pieceData().evaluateSquare(move)
-			if status == squareVacant || status == squareOccupiedByOpponent {
-				moves = append(moves, move)
-			}
-			proceedUpRight = (status == squareVacant)
-		}
-
-		// Evaluate the next move down and to the right
-		if proceedDownRight {
-			move := &Square{x: start.x + i, y: start.y - i}
-			status := b.pieceData().evaluateSquare(move)
-			if status == squareVacant || status == squareOccupiedByOpponent {
-				moves = append(moves, move)
-			}
-			proceedDownRight = (status == squareVacant)
-		}
-	}
-
-	return moves
+	return b.pieceData().generateDiagonalMoves(start)
 }
--- rook.go
+++ rook.go
@@ -13,57 +13,5 @@ func (r Rook) pieceData() *PieceData {
 }
 
 func (r Rook) generateValidMoves(start Square) []*Square {
-	moves := make([]*Square, 0)
-
-	proceedUp := true
-	proceedDown := true
-	proceedLeft := true
-	proceedRight := true
-
-	for i := startSquare; i <= endSquare; i++ {
-		// Evaluate the next move up.
-		if proceedUp {
-			move := &Square{x: start.x, y: start.y + i}
-			status := r.pieceData().evaluateSquare(move)
-			if status == squareVacant || status == squareOccupiedByOpponent {
-				moves = append(moves, move)
-			}
-
-			// You can only proceed onward if you're looking at a vacant square.
-			proceedUp = (status == squareVacant)
-		}
-
-		// Evaluate the next move down.
-		if proceedDown {
-			move := &Square{x: start.x, y: start.y - i}
-			status := r.pieceData().evaluateSquare(move)
-			if status == squareVacant || status == squareOccupiedByOpponent {
-				moves = append(moves, move)
-			}
-
-			proceedDown = (status == squareVacant)
-		}
-
-		// Evaluate the next move right.
-		if proceedRight {
-			move := &Square{x: start.x + i, y: start.y}
-			status := r.pieceData().evaluateSquare(move)
-			if status == squareVacant || status == squareOccupiedByOpponent {
-				moves = append(moves, move)
-			}
-			proceedRight = (status == squareVacant)
-		}
-
-		// Evaluate the next move left.
-		if proceedLeft {
-			move := &Square{x: start.x - i, y: start.y}
-			status := r.pieceData().evaluateSquare(move)
-			if status == squareVacant || status == squareOccupiedByOpponent {
-				moves = append(moves, move)
-			}
-			proceedLeft = (status == squareVacant)
-		}
-	}
-
-	return moves
+	return r.pieceData().generateStraightMoves(start)
 }
