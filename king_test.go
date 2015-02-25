fc1da0f29e99ab477ce1f9387ff93b424c9f1729
Cody Powell <codypo@gmail.com>
2015-02-25 06:11:19 +0000

    Forgot to add tests for king.

---
+++ king_test.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestKingGeneratesValidMoves(t *testing.T) {
+	b := NewBoard()
+	white := b.getPlayer(White)
+	king1, _ := white.getPieceByCoordinate(5, 1)
+
+	// Initially, the white king can't move because it's boxed in.
+	moves := king1.generateValidMoves(Square{x: 5, y: 1})
+	if len(moves) > 0 {
+		t.Errorf("King in starting position has valid moves.")
+	}
+
+	// Sneakiness to force a move without validation.
+	moves = king1.generateValidMoves(Square{x: 4, y: 4})
+
+	// We selected a starting square so we can move in all directions.
+	expMoves := []*Square{
+		&Square{x: 5, y: 4},
+		&Square{x: 3, y: 4},
+		&Square{x: 4, y: 5},
+		&Square{x: 4, y: 3},
+		&Square{x: 5, y: 3},
+		&Square{x: 5, y: 5},
+		&Square{x: 3, y: 5},
+		&Square{x: 3, y: 3},
+	}
+
+	arraysMatch, err := squareArraysMatch(moves, expMoves[0:])
+	if !arraysMatch {
+		t.Errorf(err.Error())
+	}
+}
