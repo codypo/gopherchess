b0239c28922879702e695d3c9b915a984a9824e0
Cody Powell <codypo@gmail.com>
2015-02-27 15:04:50 +0000

    Oops.

---
+++ pawn_test.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestPawnGeneratesValidMoves(t *testing.T) {
+	b := NewBoard()
+	white := b.getPlayer(White)
+	pawn1, _ := white.getPieceByCoordinate(2, 2)
+
+	// Pawns aren't immediately blocked by other pieces. Go nuts, pawns!
+	genMoves := pawn1.generateMoves(Square{x: 2, y: 2})
+	startMoves := []*Square{
+		&Square{x: 2, y: 3},
+		&Square{x: 2, y: 4},
+	}
+
+	arraysMatch, err := squaresMatch(genMoves, startMoves[0:])
+	if !arraysMatch {
+		t.Errorf(err.Error())
+	}
+
+	// Go ahead and move the pawn one spot forward.  With its first move made,
+	// it can no longer move forward 2 spots.
+	pawn1.pieceData().move(startMoves[0])
+
+	secondMoves := []*Square{&Square{x: 2, y: 4}}
+	genMoves = pawn1.generateMoves(*pawn1.pieceData().getSquare())
+
+	arraysMatch, err = squaresMatch(genMoves, secondMoves[0:])
+	if !arraysMatch {
+		t.Errorf(err.Error())
+	}
+}
